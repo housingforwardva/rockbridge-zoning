2f4d135343082a1ed5ca13e271b10b13c76fa442
ericvmai <30978780+ericvmai@users.noreply.github.com>
2023-02-07 20:47:28 -0500

    Update rockbridge.R
    Fiddling with spatial calculations. Seems to work.

--- r/rockbridge.R
+++ r/rockbridge.R
@@ -3,6 +3,8 @@
 library(tidyverse)
 library(sf)
 library(mapview)
+library(units)
+library(leaflet)
 
 # Import Rockbridge County parcel shapefile
 
@@ -12,5 +14,21 @@ rock_parcels <- st_read("data/rockbridge/ParcelsNovember2022.shp")
 
 rock_zoning <- st_read("data/rockbridge/RockbridgeZoning20221003.shp")
 
-mapview(rock_zoning)
+# Below dissolves polygons by zoning district and sums the acre field, which may be incorrect.
+
+rock_zsimplified <- rock_zoning %>% 
+  group_by(ZONING) %>% 
+  summarise(acres = sum(ACRES)) 
+
+# Below creates a column that calculates area based on the units set.
+
+rock_zsimplified$area <- set_units(st_area(rock_zsimplified), "acre")
+
+mapview(rock_zsimplified, zcol = "ZONING")
+
+rock_zmap <- rock_zsimplified %>% st_transform(crs = 4326)
+
+leaflet(rock_zmap) %>% 
+  addTiles() %>% 
+  addPolygons()
 
