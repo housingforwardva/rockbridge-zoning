76d69b254440d337bef47864597fe5c5ef7dcf18
ericvmai <ericvmai@gmail.com>
2023-02-22 12:06:35 -0500

    Update rockbridge.R

--- r/rockbridge.R
+++ r/rockbridge.R
@@ -12,18 +12,27 @@ rock_parcels <- st_read("data/rockbridge/Rockbridge_ParcelsFebruary2023.shp")
 
 # Import Rockbridge County zoning shapefile
 
-rock_zoning <- st_read("data/rockbridge/RockbridgeZoning20221003.shp")
+rock_zoning <- st_read("data/rockbridge/Rockbridge_Zoning20230126.shp")
+
+# Join parcels to zoning. 
+rock_parcels_z <- st_join(rock_parcels, rock_zoning, join = st_within, left = TRUE)
+
 
 # Below dissolves polygons by zoning district and sums the acre field, which may be incorrect.
 
 rock_zsimplified <- rock_zoning %>% 
   group_by(ZONING) %>% 
-  summarise(acres = sum(ACRES)) 
+  summarise(acres = sum(ACRES)) %>% 
+  filter(ZONING != "City/Town") # Filter out the towns and cities.
+  
 
 # Below creates a column that calculates area based on the units set.
 
 rock_zsimplified$area <- set_units(st_area(rock_zsimplified), "acre")
 
+rock_zsimplified_pct <- rock_zsimplified %>% 
+  mutate(pct = area/sum(area))
+  
 mapview(rock_zsimplified, zcol = "ZONING")
 
 rock_zmap <- rock_zsimplified %>% st_transform(crs = 4326)
@@ -40,4 +49,4 @@ rock_water <- st_read(dsn = "data/rockbridge/RockbridgePSA.gdb", layer = "County
 
 rock_sewer <- st_read(dsn = "data/rockbridge/RockbridgePSA.gdb", layer = "County_Sewer_20130522")
 
-mapview(rock_water)
+mapview(rock_water, color = "blue") + mapview(rock_sewer, color = "lightblue")  + mapview(rock_parcels, zcol = "MZONE")
