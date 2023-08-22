fb6784b812e9aaf561b9d9a1f80ae3b5d31c67f2
jtknopf <jonathan@hdadvisors.net>
2023-08-22 08:56:30 -0400

    add bv zoning districts

--- r/buena-vista.R
+++ r/buena-vista.R
@@ -1,30 +1,48 @@
-# -------------------------------------
-#
-# Buena Vista City data prep
-#
-# -------------------------------------
+## -------------------------------------
+##
+## Buena Vista City data prep
+##
+## -------------------------------------
 
-# Load libraries ----------------------
+## Setup -------------------------------
 
+# Load libraries
 library(tidyverse)
+library(tigris)
 library(sf)
 library(mapview)
+library(units)
+library(leaflet)
+
+# Turn spherical geometry off
+sf_use_s2(FALSE)
 
 # Import data -------------------------
 
 # Parcels
-
 bv_parcels <- st_read("data/buena-vista/Parcel20211201.shp") |>
   
   # Remove streets and river
+  filter(!OBJECTID %in% c(3621, 3622, 3648, 3649)) |> 
   
-    filter(!OBJECTID %in% c(3621, 3622, 3648, 3649))
+  # Keep only necessary columns
+  select(1, 2, 6, 15:20) |> 
+  
+  # Project to NAD83
+  st_transform(4269)
 
 # Zoning
+bv_zoning <- st_read("data/buena-vista/Zoning20211201.shp") |> 
 
-bv_zoning <- st_read("data/buena-vista/Zoning20211201.shp")
+  # Keep only necessary columns
+  select(2, 4:5, 8) |>
 
+  # Project to NAD83
+  st_transform(4269)
+  
 # View data ---------------------------
 
 mapview(bv_parcels, zcol = "CLASS")
+
+## Prep parcels for analysis -----------
 
